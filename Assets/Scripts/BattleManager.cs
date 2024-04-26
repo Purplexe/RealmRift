930fa67bd84d399e02c0c642f56898de0045eb71
Purplexe <113139597+Purplexe@users.noreply.github.com>
2024-04-25 23:20:12 -0400

    ResolvingMerge

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+public enum BattleState { Start, PlayerTurn, EnemyTurn, Won, Lost }
 
 public class BattleManager : MonoBehaviour
 {
-   
-   
+    public BattleState state;
+    public TMP_Text enemyName;
+
+    void Start()
+    {
+        state = BattleState.Start;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+
+    }
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,25 @@
+//Zach Rhodes | 4/25/2024 | Game Manager Script
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance = null;
+<<<<<<< HEAD
+    //Singleton Initiation
+=======
 
+    public float maxHealth;
+    public float currentHealth;
+    public float force;
+    public float armor;
+    public float fortitude;
+
+
+>>>>>>> dc3bdec1b19708d8415ec4fda82c62a6c0be7360
     private void Awake()
     {
         if (instance == null)
@@ -16,15 +29,15 @@ public class GameManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+<<<<<<< HEAD
 
-    // Update is called once per frame
-    void Update()
+
+    public void InitiateFight()
     {
-        
+        Debug.Log("Fight Started!");
+        SceneManager.LoadScene("BattleStage");
     }
+=======
+  
+>>>>>>> dc3bdec1b19708d8415ec4fda82c62a6c0be7360
 }
